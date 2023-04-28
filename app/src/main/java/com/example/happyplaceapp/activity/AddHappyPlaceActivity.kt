8e22552f0f308f17064a77befe6934ae9a605924
nurik17 <91252221+nurik17@users.noreply.github.com>
2023-04-28 15:42:39 +0600

    182 Setting Up the Happy Place App

---
+++ app/src/main/java/com/example/happyplaceapp/activity/AddHappyPlaceActivity.kt
@@ -0,0 +1,13 @@
+package com.example.happyplaceapp.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.happyplaceapp.R
+
+class AddHappyPlaceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_happy_place)
+
+    }
+}
