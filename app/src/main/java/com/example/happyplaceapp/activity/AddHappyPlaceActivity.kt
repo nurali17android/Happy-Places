d4753c928d16d061ae21f814872c0f93d5251bcc
nurik17 <91252221+nurik17@users.noreply.github.com>
2023-04-28 16:07:49 +0600

    183 Adding the AddHappyPlaceActivity and using dimens

--- app/src/main/java/com/example/happyplaceapp/activity/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/example/happyplaceapp/activity/AddHappyPlaceActivity.kt
@@ -3,11 +3,20 @@ package com.example.happyplaceapp.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.happyplaceapp.R
+import com.example.happyplaceapp.databinding.ActivityAddHappyPlaceBinding
 
 class AddHappyPlaceActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAddHappyPlaceBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_happy_place)
+        binding = ActivityAddHappyPlaceBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        setSupportActionBar(binding.toolbarAddPlace)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true) //back btn
+
+        binding.toolbarAddPlace.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
---
+++ app/src/main/java/com/example/happyplaceapp/activity/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.happyplaceapp.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.happyplaceapp.R
+import com.example.happyplaceapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.fabAddHappyPlace.setOnClickListener{
+            val intent = Intent(this,AddHappyPlaceActivity::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+}
